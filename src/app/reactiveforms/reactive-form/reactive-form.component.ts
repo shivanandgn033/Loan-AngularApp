12d1ea26ab737811bb5c4dabb115d11d36ec28d1
shivanandgn033 <shivanandgn033@gmail.com>
2025-02-18 21:41:02 +0530

    Update reactive form component

---
+++ src/app/reactiveforms/reactive-form/reactive-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-form.component.html',
+  styleUrl: './reactive-form.component.css'
+})
+export class ReactiveFormComponent {
+onSubmit() {
+console.log(this.reactiveform.value);
+}
+  reactiveform: FormGroup;
+  constructor() {
+    this.reactiveform = new FormGroup({
+      firstname: new FormControl(''),
+      lastname: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      isChecked: new FormControl('')
+  });
+
+}
+}
